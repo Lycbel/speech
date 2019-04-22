973e75d780efda2ac5b50578fd8ebbfbe00c5d0b
andy <andy@andydeMac-mini.local>
2019-04-22 11:56:58 +0800

    the version of top nav bar

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,18 +17,17 @@ module.exports = {
   ],
   // add your custom rules here
   'rules': {
+    'semi': 0,
+    'eqeqeq': 0,
+    'indent': 'off',
+    'no-unused-vars': 0,
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
     'generator-star-spacing': 0,
-    'no-multiple-empty-lines': [2, {'max': 4}],
-    // allow funName() {} & funName () {} & function() {}
-    'space-before-function-paren': ['error', {
-      'anonymous': 'ignore',
-      'named': 'ignore',
-      'asyncArrow': 'always'
-    }],
+    "no-extra-semi": 0,                       //多余的分号
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'space-before-function-paren': 0
   }
 }
